2ec983288ab1d467b720a162003b1b74ab776445
XLYN529 <130530448+XLYN529@users.noreply.github.com>
2024-11-03 21:08:49 -0500

    Update firebase.js

--- StudySpace/src/firebase.js
+++ StudySpace/src/firebase.js
@@ -12,14 +12,14 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- // apiKey: "AIzaSyBzyA_vDdhaIhBRa5cHXc5EIPl3-vGeXmo",
+ apiKey: "",
   authDomain: "studyspace-ba88a.firebaseapp.com",
   projectId: "studyspace-ba88a",
   storageBucket: "studyspace-ba88a.appspot.com",
   messagingSenderId: "1008594304935",
-//  appId: "1:1008594304935:web:e756fff7bc38cac99e8082",
+  appId: "1:1008594304935:web:e756fff7bc38cac99e8082",
   measurementId: "G-EM4EFG3W4S",
- // databaseURL: "https://studyspace-ba88a-default-rtdb.firebaseio.com"
+  databaseURL: ""
 };
 
 // Initialize Firebase
